151036ba0c55a4e7e51e3ac6b22adbb8e3a1d033
virajsirohi580 <77614067+virajsirohi580@users.noreply.github.com>
2021-06-20 12:53:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,128 @@
+//Examples on different types of data in javascript
+
+//String
+var name = " Viraj Sirohi!"
+console.log(name);
+
+//Number
+var num= 100;
+console.log(num);
+
+//Boolean
+var bool = true;
+console.log(bool);
+
+//undefined
+var object;
+console.log(object);
+
+//Reassigning the same undefined object to null
+//null
+object=null;
+console.log(object);
+
+
+//Examples of an array
+var array1 = [1,2,3,4,5,6];
+console.log(array1);
+
+var array2= [ "Viraj" , 27, true];
+console.log(array2);
+
+var array3= [ [1,2,3] , [2,8,9,9] , [4,5,8,5]  ]
+console.log(array3);
+
+console.log(array3[1][3])
+
+array3.push("Viraj")
+console.log(array3);
+
+array3.pop();
+console.log(array3);
+
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1,pig3;
+var backgroundImg,platform;
+var bird, slingshot;
+var gameState= "onSling"
+
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(200,50);
+
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
+}
+
+function draw(){
+    background(backgroundImg);
+    Engine.update(engine);
+    //strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig3.display();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
+    platform.display();
+    //log6.display();
+    slingshot.display();    
+}
+
+function mouseDragged(){
+    if(gameState!=="launched"){
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+}
+}
+
+function mouseReleased(){
+    gameState="launched"
+    slingshot.fly();
+}
+
+function keyPressed(){
+    if(keyCode === 32){
+        //slingshot.attach(bird.body);
+    }
+}
